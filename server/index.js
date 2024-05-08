eb3bdde14dfa1cd00923de2670b2004d2de17be3
Om <omghate9@gmail.com>
2024-05-08 14:45:04 +0530

    Commit Safe

--- server/index.js
+++ server/index.js
@@ -5,19 +5,18 @@ const cors = require("cors");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
-const userResultRoutes = require("./routes/usersResult")
+const userResultRoutes = require("./routes/usersResult");
 // database connection
 connection();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/usersResult", userResultRoutes);
-
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
