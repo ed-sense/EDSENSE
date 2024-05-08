f06b446ee310514e9332ad6e00172b8b69e7f896
Om <omghate9@gmail.com>
2024-05-08 21:40:14 +0530

    Commit Changes

--- server/models/userResult.js
+++ server/models/userResult.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 
 const userResultSchema = new mongoose.Schema({
-	email: { type: String, required: true },
-    Q_arr: { type: Array, required: true },
-    Timer: {type: Array, required: true },
-    R_ans: { type: Array, required: true },
-    SpW_ans: { type: Array, required: true },
-    W_ans: { type: Array, required: true },
-    // Result: {type: String, required: true}
-
+  email: { type: String, required: true },
+  Q_arr: { type: Array, required: true },
+  Timer: { type: Array, required: true },
+  R_ans: { type: Array, required: true },
+  SpW_ans: { type: Array, required: true },
+  W_ans: { type: Array, required: true },
+  // Result: {type: String, required: true}
 });
 const userResult = mongoose.model("userResult", userResultSchema);
 
